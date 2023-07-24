840c76cf3615c56df4a633da80463ffd90464a79
Samantha <samantha.fenton@live.co.uk>
2023-07-24 15:26:35 +0100

    added the handlemap function and linked the data from location iq now checking the return

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import "./App.css";
 function App() {
   const [searchQuery, setSearchQuery] = useState();
   const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
 
   function handleSearch(event) {
     setSearchQuery(event.target.value);
   }
 
-  function getLocation(event) {
+  async function getLocation(event) {
     const API =
       "https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json";
+    const res = await axios.get(API);
+    setLocation(res.data[0]);
+    handleMap(res.data[0]);
+  }
+
+  function handleMap(data) {
+    const API =
+      "GET https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data,lat},${data,lon}&zoom=9";
+    setMap(API);
   }
 
   return (
     <div className="App">
       <input type="text" placeholder="search for a city" onChange={handleSearch}></input>
       <button onClick={getLocation}>Explore</button>
+      <p>{location.display_name}</p>
     </div>
   );
 }
