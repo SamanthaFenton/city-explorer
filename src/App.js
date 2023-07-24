4bd1741a75fa974c5a74fab1316bbe489adb6e31
Samantha <samantha.fenton@live.co.uk>
2023-07-24 15:43:04 +0100

    making amendments trying to render map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState();
@@ -8,11 +9,13 @@ function App() {
 
   function handleSearch(event) {
     setSearchQuery(event.target.value);
+    // console.log(searchQuery);
   }
 
   async function getLocation(event) {
     const API =
       "https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json";
+    // console.log(API);
     const res = await axios.get(API);
     setLocation(res.data[0]);
     handleMap(res.data[0]);
@@ -26,9 +29,12 @@ function App() {
 
   return (
     <div className="App">
-      <input type="text" placeholder="search for a city" onChange={handleSearch}></input>
-      <button onClick={getLocation}>Explore</button>
+      <form onSubmit={getLocation}>
+        <input type="text" placeholder="search for a city" onChange={handleSearch}></input>
+        <button onClick={getLocation}>Explore</button>
+      </form>
       <p>{location.display_name}</p>
+      {map && <img src={map} alt="map" />}
     </div>
   );
 }
