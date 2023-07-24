a8aed165fa9bf17511648e0d9b894e745de4ba3e
Samantha <samantha.fenton@live.co.uk>
2023-07-24 14:38:47 +0100

    added a search function and part way through the function to explore prior to adding API data, saving prior to viewing in browser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useSyncExternalStore } from "react";
+import "./App.css";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState();
+  const [location, setLocation] = useState({});
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  function getLocation(event) {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="search for a city" onChange={handleSearch}></input>
+      <button onClick={getLocation}>Explore</button>
     </div>
   );
 }
